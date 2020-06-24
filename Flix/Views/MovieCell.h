34b83580b32eedd6a2f77cf5c96e7dc167461de6
Angela Xu <xuangela@cs.washington.edu>
2020-06-24 16:30:19 -0700

    2. table view of current movies

---
+++ Flix/Views/MovieCell.h
@@ -0,0 +1,21 @@
+//
+//  MovieCell.h
+//  Flix
+//
+//  Created by Angela Xu on 6/24/20.
+//  Copyright © 2020 Angela Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
